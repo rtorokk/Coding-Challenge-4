8dbbca53b59266aa494daa5cc34beffc0ed038d1
Rachael <rachy.torok@gmail.com>
2025-01-31 09:58:13 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+// Task 1: If Statements - Customer Discounts
+
+let purchaseAmount = 100;// Enter the amount of purchase
+if (purchaseAmount > 100) 
+purchaseAmount = 90;// Enter the amount of purchase
+let finalAmount = purchaseAmount - 20;// Enter the amount of purchase
+console.log(`Final amount after discount: $${finalAmount}`); // Output: Final amount after discount: $80

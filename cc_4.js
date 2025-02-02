a84172faee00c22d7f7550571eb814ad04f628a7
Rachael <rachy.torok@gmail.com>
2025-02-01 23:00:45 -0500

    Task 2- Sales Report

--- cc_4.js
+++ cc_4.js
@@ -5,3 +5,11 @@ if (purchaseAmount > 100)
 purchaseAmount = 90;// Enter the amount of purchase
 let finalAmount = purchaseAmount - 20;// Enter the amount of purchase
 console.log(`Final amount after discount: $${finalAmount}`); // Output: Final amount after discount: $80
+
+
+// Task 2: For Loop - Sales Report
+
+let sales = [100, 230, 55, 75, 120];// Enter the sales data
+for (let i = 0; i < sales.length; i++) {
+  console.log(`Total Sales/ day: $${sales[i]}`); // Output: Day 1: $100, Day 2: $230, Day 3: $55, Day 4: $75, Day 5: $120
+}
